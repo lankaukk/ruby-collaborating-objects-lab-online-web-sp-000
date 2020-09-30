fcc87c652ad500c184122ed66d505993b494e703
McKayla Lankau <lankaukk@yahoo.com>
2020-09-30 15:06:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    self.save
+
   end
 
   def add_song(song)
@@ -27,6 +27,7 @@ class Artist
       found_artist
     else
       new_artist = self.new(artist_name)
+      new_artist.save
       new_artist
     end
   end
